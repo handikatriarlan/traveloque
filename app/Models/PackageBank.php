d3936d2b036026acea746b8fbbd1bab222c17f3e
Arlann <handikaarlan@gmail.com>
2024-11-02 20:35:45 +0700

    feat: add traits and relations in models

--- app/Models/PackageBank.php
+++ app/Models/PackageBank.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PackageBank extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'bank_name',
+        'bank_account_name',
+        'bank_number_name',
+        'logo',
+    ];
 }
