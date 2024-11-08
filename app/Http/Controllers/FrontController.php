01de8dfa19ccdda7839efe361fd6fef1ab95f2c8
Arlann <handikaarlan@gmail.com>
2024-11-09 00:05:39 +0700

    Added book payment and finish routes to FrontController, updated PackageBooking model relationships

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePackageBookingCheckoutRequest;
 use App\Http\Requests\StorePackageBookingRequest;
 use App\Http\Requests\UpdatePackageBookingRequest;
 use App\Models\PackageBank;
@@ -101,7 +102,43 @@ class FrontController extends Controller
             ]);
         });
 
-        return redirect()->route('front.book_payment', $packageBooking->id);
+        return redirect()->route('front.book-payment', $packageBooking->id);
+    }
+
+    public function book_payment(PackageBooking $packageBooking)
+    {
+        return view('front.book-payment', compact('packageBooking'));
+    }
+
+    public function book_payment_store(StorePackageBookingCheckoutRequest $request, PackageBooking $packageBooking)
+    {
+        $user = Auth::user();
+
+        if ($packageBooking->user_id != $user->id) {
+            abort(403);
+        }
+
+        DB::transaction(function () use ($request, $user, $packageBooking) {
+            $validated = $request->validated();
+
+            if ($request->hasFile('proof')) {
+                $proofPath = $request->file('proof')->store('proofs', 'public');
+                $validated['proof'] = $proofPath;
+            }
+
+            $packageBooking->update($validated);
+            // $packageBooking->update([
+            //     'is_paid' => true,
+            // ]);
+
+        });
+
+        return redirect()->route('front.book-finish');
+    }
+
+    public function book_finish()
+    {
+        return view('front.book-finish');
     }
 
     /**
