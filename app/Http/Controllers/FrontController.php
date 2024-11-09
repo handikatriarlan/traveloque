c25a1b4692c36f7777272eb18c52c0782ed6999d
Arlann <handikaarlan@gmail.com>
2024-11-09 18:48:09 +0700

    Removed unused CRUD methods from DashboardController and FrontController

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -15,9 +15,6 @@ use Illuminate\Support\Facades\DB;
 
 class FrontController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $packageTours = PackageTour::orderByDesc('id')->take(3)->get();
@@ -139,53 +136,5 @@ class FrontController extends Controller
     public function book_finish()
     {
         return view('front.book-finish');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
